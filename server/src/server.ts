b7cc81a543413c8d4d1b0649105dcc3011f588bd
Tomas Cuevas <memo_mister398@hotmail.com>
2023-02-17 14:45:22 -0300

    chore: :construction: set first origin on cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -39,10 +39,10 @@ class Server {
   private middleware() {
     this.app.use(express.json());
     this.app.use(fileUpload({ useTempFiles: true, tempFileDir: "/tmp/" }));
-    this.app.use(cors({ origin: origins, credentials: true }));
-    this.app.options("*", cors({ origin: origins, credentials: true }));
+    this.app.use(cors({ origin: origins![0], credentials: true }));
+    this.app.options("*", cors({ origin: origins![0], credentials: true }));
     this.app.use((req, res, next) => {
-      res.header("Access-Control-Allow-Origin", origins);
+      res.header("Access-Control-Allow-Origin", origins![0]);
       res.header("Access-Control-Allow-Credentials", "true");
       res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
       res.header(
