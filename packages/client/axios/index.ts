34b2b7cd7265dd653ede7a08e3aae125233a3d4f
Tomas Cuevas <memo_mister398@hotmail.com>
2023-04-21 22:20:11 -0300

    feat(client): :sparkles: add index file on axios folder

---
+++ packages/client/axios/index.ts
@@ -0,0 +1,4 @@
+export { default as authApi } from "./authApi";
+export { default as postApi } from "./postApi";
+export { default as settingsApi } from "./settingsApi";
+export { default as userApi } from "./userApi";
