e2e487bad6e9016809a8c43794be121c04079be4
Tomas Cuevas <memo_mister398@hotmail.com>
2023-04-24 00:37:11 -0300

    refactor(client): :recycle: remove providers and add stores on _app file

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -1,43 +1,39 @@
+import { useEffect } from "react";
 import { AppProps } from "next/app";
 import Head from "next/head";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-//* providers *//
-import {
-  AuthProvider,
-  UIProvider,
-  DataProvider,
-  RightSidebarProvider,
-} from "../context";
+//* stores *//
+import { useAuthStore, usePostsStore } from "@/store";
 
 //* styles *//
 import "../styles/globals.css";
 import "../styles/editor.css";
 import "../styles/post.css";
 
-const queryClient = new QueryClient();
+//* query client *//
+export const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { isAuthenticated, onChecking } = useAuthStore();
+  const { onGetSavedPostsList } = usePostsStore();
+
+  useEffect(() => {
+    onChecking();
+  }, []);
+
+  useEffect(() => {
+    onGetSavedPostsList();
+  }, [isAuthenticated]);
+
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools />
-      <AuthProvider>
-        <UIProvider>
-          <RightSidebarProvider>
-            <DataProvider>
-              <Head>
-                <link
-                  rel="shortcut icon"
-                  href="/evlun-logo.svg"
-                  type="image/x-icon"
-                />
-              </Head>
-              <Component {...pageProps} />
-            </DataProvider>
-          </RightSidebarProvider>
-        </UIProvider>
-      </AuthProvider>
+      <Head>
+        <link rel="shortcut icon" href="/evlun-logo.svg" type="image/x-icon" />
+      </Head>
+      <Component {...pageProps} />
     </QueryClientProvider>
   );
 }
