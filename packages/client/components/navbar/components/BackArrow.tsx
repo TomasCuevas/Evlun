4bf74498c8206d65a80de77dfa1e372b4c0df5d6
Tomas Cuevas <memo_mister398@hotmail.com>
2023-04-27 13:49:03 -0300

    refactor(client): :recycle: move BackArrow, NavText and NavTextButton to component folder on Navbar component

--- packages/client/components/navbar/components/BackArrow.tsx
+++ packages/client/components/navbar/components/BackArrow.tsx
@@ -3,14 +3,11 @@ import { useRouter } from "next/router";
 //* icons *//
 import { BsArrowLeftShort } from "react-icons/bs";
 
-export const BackArrow = () => {
-  const router = useRouter();
+export const BackArrow: React.FC = () => {
+  const { back } = useRouter();
 
   return (
-    <div
-      className="flex items-center justify-start"
-      onClick={() => router.back()}
-    >
+    <div className="flex items-center justify-start" onClick={() => back()}>
       <BsArrowLeftShort className="cursor-pointer text-3xl text-orange/70 hover:text-orange" />
     </div>
   );
