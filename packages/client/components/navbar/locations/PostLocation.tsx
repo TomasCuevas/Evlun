43a9be0b3ec4937dae1f9bf549174726ee806b56
Tomas Cuevas <memo_mister398@hotmail.com>
2023-04-27 13:53:00 -0300

    refactor(client): :recycle: rename NavTopPost for PostLocation

---
+++ packages/client/components/navbar/locations/PostLocation.tsx
@@ -0,0 +1,13 @@
+//* components *//
+import { BackArrow } from "@/components/navbar";
+
+export const PostLocation: React.FC = () => {
+  return (
+    <nav className="mx-auto flex h-full w-full items-center gap-[30px]">
+      <div className="flex h-full w-full items-center gap-[20px]">
+        <BackArrow />
+        <span className="font-bold text-orange">Post</span>
+      </div>
+    </nav>
+  );
+};
