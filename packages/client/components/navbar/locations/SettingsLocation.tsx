ed7118ef0be366cb9d57cec6afc3794408966ef7
Tomas Cuevas <memo_mister398@hotmail.com>
2023-04-27 13:54:47 -0300

    refactor(client): :recycle: rename NavTopSettings for SettingsLocation and replace contexts for stores

---
+++ packages/client/components/navbar/locations/SettingsLocation.tsx
@@ -0,0 +1,24 @@
+//* components *//
+import { BackArrow, NavTextButton, NavText } from "@/components/navbar";
+
+//* store *//
+import { useAuthStore, useNavbarTopStore } from "@/store";
+
+export const SettingsLocation: React.FC = () => {
+  const { user } = useAuthStore();
+  const { navbarData } = useNavbarTopStore();
+
+  const { settingText, buttonOnClick, buttonText } = navbarData;
+
+  return (
+    <nav className="mx-auto flex h-full w-full items-center gap-[30px]">
+      <div className="flex h-full w-full items-center gap-[20px]">
+        <BackArrow />
+        <NavText textBig={settingText!} textSmall={`@${user?.username}`} />
+        {navbarData.isButton ? (
+          <NavTextButton text={buttonText!} onClick={buttonOnClick} />
+        ) : null}
+      </div>
+    </nav>
+  );
+};
