128f672fab24eaa7f0d453ba7eb34860d96837ef
Tomas Cuevas <memo_mister398@hotmail.com>
2023-05-09 05:37:39 -0300

    feat(client): :sparkles: add new method on user store

--- packages/client/store/user.store.ts
+++ packages/client/store/user.store.ts
@@ -8,11 +8,15 @@ import { IUser } from "@/interfaces";
 
 interface useUserState {
   userUpdated?: IUser;
+  clearUser(): void;
   getUser(username: string): void;
 }
 
 export const useUserStore = create<useUserState>((set) => ({
   userUpdated: undefined,
+  clearUser() {
+    set(() => ({ userUpdated: undefined }));
+  },
   async getUser(username: string) {
     const result = await getUserService(username);
     if (result.ok) {
