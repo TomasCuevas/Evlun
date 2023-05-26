7cacabd15f289a5c7445586450e9492eb60ac74a
Tomas Cuevas <memo_mister398@hotmail.com>
2023-05-25 22:59:09 -0300

    chore(client): :wrench: add new color on tailwind config

--- packages/client/tailwind.config.js
+++ packages/client/tailwind.config.js
@@ -11,9 +11,10 @@ module.exports = {
       colors: {
         yellow: "#f9f785",
         background: "#15202b",
+        grayLight: "##1e2732",
         light: "#1e2732",
         orange: "#e86d33",
-        error: "#FF2222",
+        error: "#fa0d20",
       },
       screens: {
         xs: "500px",
