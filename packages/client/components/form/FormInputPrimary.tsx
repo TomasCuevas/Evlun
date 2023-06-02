3ccb6b484b9b1b6494d104b13a1a8f817633d15b
Tomas Cuevas <memo_mister398@hotmail.com>
2023-06-02 13:13:25 -0300

    feat(client): :sparkles: add button to show password on FormInputPrimary

--- packages/client/components/form/FormInputPrimary.tsx
+++ packages/client/components/form/FormInputPrimary.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { BsEye, BsEyeSlash } from "react-icons/bs";
 
 //* interface *//
 interface Props {
@@ -18,8 +19,9 @@ export const FormInputPrimary: React.FC<Props> = ({
   label,
   max,
 }) => {
+  const [viewPassword, setViewPassword] = useState(false);
   const [focus, setFocus] = useState(false);
-  const inputFocus = () => setFocus(true);
+
   const inputBlur = () => {
     if (typeof inputValue === "string" && inputValue.length < 1) {
       setFocus(false);
@@ -41,23 +43,35 @@ export const FormInputPrimary: React.FC<Props> = ({
         htmlFor={inputName}
         className={
           focus
-            ? "absolute top-[5px] text-xs font-light text-orange transition-all duration-300"
-            : "absolute font-medium text-orange transition-all duration-300"
+            ? "absolute top-[5px] text-xs font-light text-orange"
+            : "absolute font-semibold text-orange"
         }
       >
         {label}
       </label>
       <input
         maxLength={max}
-        onFocus={inputFocus}
+        onFocus={() => setFocus(true)}
         onBlur={inputBlur}
-        type={inputType}
+        type={viewPassword ? "text" : inputType}
         name={inputName}
         id={inputName}
         value={inputValue}
         onChange={inputChange}
-        className="w-full border-none bg-[#0000] pt-[10px] text-lg font-medium text-background outline-none autofill:!bg-[#0000]"
+        className="w-full border-none bg-[#0000] pt-[10px] text-lg font-medium text-background outline-none autofill:bg-[#0000]"
       />
+      {inputType === "password" &&
+        (viewPassword ? (
+          <BsEyeSlash
+            onClick={() => setViewPassword(false)}
+            className="mt-1 cursor-pointer text-2xl text-orange"
+          />
+        ) : (
+          <BsEye
+            onClick={() => setViewPassword(true)}
+            className="mt-1 cursor-pointer text-2xl text-orange"
+          />
+        ))}
     </div>
   );
 };
