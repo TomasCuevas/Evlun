db7076eebb7bb03c7960193180650fc70f83aa4e
Tomas Cuevas <memo_mister398@hotmail.com>
2023-06-06 16:25:24 -0300

    style(client): :lipstick: update style on Loader component

--- packages/client/components/ui/Loader.tsx
+++ packages/client/components/ui/Loader.tsx
@@ -1,7 +1,7 @@
 export const Loader = () => {
   return (
-    <div className="mt-4 flex h-12 w-full items-center justify-center bg-background">
-      <span className="h-12 w-12 animate-spin rounded-full  border-t-[4px] border-r border-t-orange border-r-[#0000]"></span>
+    <div className="mt-4 flex h-8 w-full items-center justify-center bg-background">
+      <span className="h-8 w-8 animate-spin rounded-full  border-t-[4px] border-r border-t-orange border-r-[#0000]" />
     </div>
   );
 };
