23d4f7eb28f5f8f176873c9eb1412ae0965e538b
Tomas Cuevas <memo_mister398@hotmail.com>
2023-06-09 15:41:21 -0300

    feat(client): :sparkles: add SwitchPost component on HomeLocation component

--- packages/client/components/navbar/locations/HomeLocation.tsx
+++ packages/client/components/navbar/locations/HomeLocation.tsx
@@ -1,5 +1,8 @@
 import NextLink from "next/link";
 
+//* components *//
+import { SwitchPost } from "@/components/home";
+
 //* store *//
 import { useAuthStore, useUiStore } from "@/store";
 
@@ -9,24 +12,34 @@ export const HomeLocation: React.FC = () => {
 
   return (
     <nav
-      style={{ padding: "13px 16px" }}
-      className="flex h-full w-full items-center gap-[30px]"
+      style={{
+        padding: "8px 0px 0px 0px",
+      }}
+      className="mx-auto flex h-full w-full flex-col items-center gap-2"
     >
-      <div className="max-h-[30px] min-h-[30px] min-w-[30px] max-w-[30px] xs:hidden">
-        <img
-          onClick={onSwitchMobileSidebar}
-          src={user?.avatar}
-          alt="profile-img"
-          className="h-[30px] w-[30px] cursor-pointer rounded-full object-cover object-center"
-        />
-      </div>
-      <div>
-        <NextLink href="/" passHref>
-          <a className="cursor-pointer text-lg font-bold text-orange xs:text-xl">
-            Inicio
-          </a>
-        </NextLink>
-      </div>
+      <section
+        className="flex h-full w-full items-center gap-[20px]"
+        style={{ padding: "0px 5%" }}
+      >
+        <div className="max-h-[30px] min-h-[30px] min-w-[30px] max-w-[30px] xs:hidden">
+          <img
+            onClick={onSwitchMobileSidebar}
+            src={user?.avatar}
+            alt="profile-img"
+            className="h-[30px] w-[30px] cursor-pointer rounded-full object-cover object-center"
+          />
+        </div>
+        <div>
+          <NextLink href="/" passHref>
+            <a className="cursor-pointer text-lg font-bold text-orange xs:text-xl">
+              Inicio
+            </a>
+          </NextLink>
+        </div>
+      </section>
+      <section className="w-full">
+        <SwitchPost />
+      </section>
     </nav>
   );
 };
